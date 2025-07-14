c111a99eb744ed8387cdfef77c37f439c08026cc
Henrique Barbosa <henrikleod@gmail.com>
2025-07-14 12:13:20 -0300

    fix: lint issues

--- spec/factories/discount_rules.rb
+++ spec/factories/discount_rules.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :discount_rule do
-    prerequisite_skus { ["SKU-P1", "SKU-P2", "SKU-P3"] }
-    eligible_skus { ["SKU-E4", "SKU-E5", "SKU-E6"] }
+    prerequisite_skus { [ "SKU-P1", "SKU-P2", "SKU-P3" ] }
+    eligible_skus { [ "SKU-E4", "SKU-E5", "SKU-E6" ] }
     discount_unit { "percentage" }
     discount_value { 9.99 }
   end
