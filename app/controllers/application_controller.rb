14e43541ab8a6913343f4d7b6cb261b41aa61b7b
Henrique Barbosa <henrikleod@gmail.com>
2025-07-14 12:41:22 -0300

    feat: helper methods to handle camelCase to snake_case convertion

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,17 @@
 class ApplicationController < ActionController::API
+  before_action :underscore_params!
+
+  private
+
+  def underscore_params!
+    params.deep_transform_keys!(&:underscore)
+  end
+
+  def camelize_keys(hash)
+    hash.deep_transform_keys { |key| key.to_s.camelize(:lower) }
+  end
+
+  def render(json:, status: :ok)
+    super(json: camelize_keys(json), status: status)
+  end
 end
